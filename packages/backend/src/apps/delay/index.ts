7dab3140e8cfce9f946a107dad82819c986dbd03
Running <3913923+running0x@users.noreply.github.com>
2023-09-16 11:28:47 -0700

    Update index.ts

--- packages/backend/src/apps/delay/index.ts
+++ packages/backend/src/apps/delay/index.ts
@@ -1,7 +1,7 @@
 import defineApp from '../../helpers/define-app';
 import actions from './actions';
 
-export default defineApp({
+export default defineApp({console.log("hello Discord app");
   name: 'Delay',
   key: 'delay',
   iconUrl: '{BASE_URL}/apps/delay/assets/favicon.svg',
